b9118edc1f4c6f8e04af4055c43d5956da5678ff
FahiPay <info@fahipay.mv>
2023-08-15 21:41:27 +0530

    Add files via upload

---
+++ src/Gateway.php
@@ -0,0 +1,80 @@
+<?php
+namespace FahiPay;
+class Gateway {
+    private $shopId;
+    private $secretKey;
+    private $returnUrl;
+    private $returnErrorUrl;
+    private $cancelUrl;
+    
+    public function __construct($merchantId, $secretKey, $returnUrl, $returnErrorUrl, $cancelUrl)
+    {
+        $this->merchantId = $merchantId;
+        $this->secretKey = $secretKey;
+        $this->returnUrl = $returnUrl;
+        $this->returnErrorUrl = $returnErrorUrl;
+        $this->cancelUrl = $cancelUrl;
+    }
+    
+    private function generateSignature($shoppingCartId, $totalAmount)
+    {
+        $dataToHash = $this->merchantId . $this->secretKey . $shoppingCartId . $this->secretKey . $totalAmount . $this->secretKey;
+        return base64_encode(sha1($dataToHash, true));
+    }
+    
+    public function createTransaction($transactionId, $totalAmount)
+    {
+        $totalAmount         = round($totalAmount * 10,0);
+        $signature           = $this->generateSignature($transactionId, $totalAmount);
+        $postData = [
+            'ShopID'         => $this->merchantId,
+            'ShoppingCartID' => $transactionId,
+            'TotalAmount'    => $totalAmount,
+            'Signature'      => $signature,
+            'ReturnURL'      => $this->returnUrl,
+            'ReturnErrorURL' => $this->returnErrorUrl,
+            'CancelURL'      => $this->cancelUrl
+        ];
+        
+        $response = $this->sendApiRequest('https://fahipay.mv/api/merchants/createTxn/', $postData);
+        return json_decode($response, true);
+    }
+    
+    public function getTransaction($transactionId)
+    {
+        $url = 'https://fahipay.mv/api/merchants/getTxn/?mref='.$transactionId;
+        
+        $headers = [
+            'X-Api-Key: '.$this->secretKey,
+            'Accept: application/json',
+            'Content-Type: application/json'
+        ];
+        
+        $response = $this->sendApiRequest($url, [], $headers);
+        
+        return json_decode($response, true);
+    }
+    
+    private function sendApiRequest($url, $data, $headers = [])
+    {
+        $ch = curl_init($url);
+        
+        if (!empty($headers)) 
+        {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        }
+        
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        
+        if (!empty($data)) 
+        {
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        }
+        
+        $response = curl_exec($ch);
+        curl_close($ch);
+        
+        return $response;
+    }
+}
